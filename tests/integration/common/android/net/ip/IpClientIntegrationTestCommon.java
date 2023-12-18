b2dd1cdcd9e723478db6c99b4c59e56ce062a7c4
Xiao Ma <xiaom@google.com>
2023-12-18 14:59:18 +0900

    Cleanup the IPCLIENT_MULTICAST_NS_VERSION experiment flag.
    The feature has been enabled by default and rolled out via M-2023-07
    train, cleanup the constant flag string and associated dead code.
    
    Bug: 315056647
    Test: atest NetworkStackIntegrationTests
    Change-Id: I6305897cd307ebc2d65f6d623a44ec33c8abd5cc

--- tests/integration/common/android/net/ip/IpClientIntegrationTestCommon.java
+++ tests/integration/common/android/net/ip/IpClientIntegrationTestCommon.java
@@ -4664,9 +4664,6 @@ public abstract class IpClientIntegrationTestCommon {
                 .withoutIPv4()
                 .build();
 
-        setFeatureEnabled(NetworkStackUtils.IPCLIENT_MULTICAST_NS_VERSION,
-                true /* isUnsolicitedNsEnabled */);
-        assertTrue(isFeatureEnabled(NetworkStackUtils.IPCLIENT_MULTICAST_NS_VERSION));
         startIpClientProvisioning(config);
 
         doIpv6OnlyProvisioning();
